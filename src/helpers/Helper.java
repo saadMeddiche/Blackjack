2ca51193924920df1299650692691fa435e66c24
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-09-20 16:37:17 +0100

    Random Function (Testing)

--- src/helpers/Helper.java
+++ src/helpers/Helper.java
@@ -2,4 +2,24 @@ package helpers;
 
 public class Helper {
 
+    public static void main(String[] args) throws Exception {
+
+        // Integer from = 1;
+        // Integer to = 13;
+
+        // Integer multiplier = 7;
+        // Integer increment = 13;
+        // Integer modulus = 10;
+
+        // for (int i = 0; i < 100; i++) {
+        //     long time = System.currentTimeMillis();
+
+        //     int seed = (int) (time % 1000);
+
+        //     int randomNumber = (multiplier * seed + increment) % modulus;
+
+        //     System.out.println(randomNumber);
+        // }
+
+    }
 }
